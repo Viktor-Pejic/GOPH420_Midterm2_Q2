b6a825cbe255f2edee20dfd7b061152b1a866cd1
Viktor Pejic <viktor.pejic@ucalgary.ca>
2025-04-08 11:39:07 -0600

    all plots fixed

--- examples/linearized_data.py
+++ examples/linearized_data.py
@@ -15,7 +15,7 @@ def main():
     plt.xlabel("Rho [g/cm^3]")
     plt.ylabel("Log(V_0) [m/s]")
     plt.title("Linearized Plot of Data", fontweight="bold")
-    plt.savefig('../figures/Linearized_Raw_Data.png')
+    plt.savefig('../figures/Linearized_Data.png')
 
 if __name__ == '__main__':
     main()
